f4117852ae95667dffbc54bf2a55e5334a346047
Mikael Barcat <aleplay632@gmail.com>
2026-09-19 16:23:13 -0300

    Baja Fisica y Logica

--- Unidad 5/TratamientosDeArchivos.c
+++ Unidad 5/TratamientosDeArchivos.c
@@ -18,7 +18,7 @@ void alta(){
     printf("Ingrese clave: ");
     scanf("%d " , &nuevo.clave);
 
-    /**
+    /*
     printf("Ingrese nombre: ");
     fflush(stdin);
     gets(nuevo.nombre);
@@ -66,5 +66,70 @@ void modificar(){
 
     fclose(archivo);
     if(encontrado == 0) printf("Clave no encontrada o dada de baja\n");
+
+}
+
+void bajaLogica(){
+
+    FILE *archivo = fopen("archivo.dat" , "ab");
+    if(archivo == NULL) return;
+
+    struct Registro reg;
+    int encontrado = 0 , claveBuscado;
+
+    printf("Ingrese legajo a borrar: ");
+    scanf("%d", &claveBuscado);
+
+    while(fread(&reg , sizeof(struct Registro) , 1 , archivo) == 1){
+
+        if(reg.clave == claveBuscado && reg.activo == 1){
+
+            reg.activo = 0;
+            fseek(archivo , -(long)sizeof(struct Registro) , SEEK_CUR);
+            fwrite(&reg , sizeof(struct Registro) , 1 , archivo);
+            encontrado = 1;
+            printf("Baja logica empleada con exito\n");
+            break;
+
+        }
+
+    }
+
+    fclose(archivo);
+    if(encontrado == 0) printf("Clave no encontrada\n");
+
+}
+
+void bajaFisica(){
+
+    FILE *og = fopen("archivo.dat" , "rb") , *temp = fopen("temporal.dat" , "wb");
+    if(og == NULL || temp == NULL) return;
+
+    struct Registro reg;
+    int encontrado = 0;
+
+    while(fread(&reg , sizeof(struct Registro) , 1 , og) == 1){
+
+        if(reg.activo == 1) fwrite(&reg , sizeof(struct Registro) , 1 , temp);
+        else encontrado = 1;
+
+    }
+
+    fclose(og);
+    fclose(temp);
+
+    if(encontrado == 1){
+
+        remove("archivo.dat");
+        rename("temporal.dat" , "archivo.dat");
+        printf("Baja fisica de mantenimiento ejecutada con exito\n");
+
+    }
+    else{
+
+        remove("temporal.dat");
+        printf("No habia regsitros dado de baja para limpiar\n");
+
+    }
 
 }
